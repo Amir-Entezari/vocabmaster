b65b5baa6d1434819fe693d37a0aaedce521fe52
amir <mr.amirentezari@gmail.com>
2024-05-18 19:44:52 +0330

    feat(model): define Word model

--- vocabulary/admin.py
+++ vocabulary/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from vocabulary.models import Word
+
 # Register your models here.
+admin.site.register(Word)
--- vocabulary/models.py
+++ vocabulary/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Word(models.Model):
+    word = models.CharField(max_length=255)
+    meaning = models.TextField()
+    synonyms = models.ManyToManyField("self", blank=True)
+    example_sentence = models.TextField(blank=True, null=True)
+    extra_info = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.word
