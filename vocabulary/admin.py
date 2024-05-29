404edfbbbc85d6fa56c64d7672d8e3640743f1ee
amir <mr.amirentezari@gmail.com>
2024-05-30 01:18:33 +0330

    feat(admin): define admin model for Word class and UserWord class

--- vocabulary/admin.py
+++ vocabulary/admin.py
@@ -32,6 +32,16 @@ class UserAdmin(BaseUserAdmin):
     ]
 
 
-admin.site.register(Word)
+@admin.register(Word)
+class WordAdmin(admin.ModelAdmin):
+    # autocomplete_fields = ['word']
+    list_display = ['id', 'word', 'meaning']
+    search_fields = ['word']
 
-admin.site.register(UserWord)
+
+@admin.register(UserWord)
+class UserWordAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['word']
+    list_display = ['id', 'word', 'added_at', 'user_note']
+    search_fields = ['word']
+
