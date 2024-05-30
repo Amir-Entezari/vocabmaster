e1622a459a41544c55ea125ab5714fb7115490d1
amir <mr.amirentezari@gmail.com>
2024-05-30 14:33:10 +0330

    refactor(api): move users api from vocabulary app to users app

--- vocabulary/api.py
+++ vocabulary/api.py
@@ -1,50 +1,20 @@
 from datetime import datetime
-from sqlite3 import IntegrityError
 from typing import List
 
-from django.contrib.auth import login, logout
-from django.http import HttpResponse, HttpRequest
-from ninja import NinjaAPI, Router
+from django.http import HttpRequest
+from ninja import Router
 from ninja.errors import HttpError
 from ninja.orm import create_schema
-# from django.contrib.auth.models import User
 
-from .auth import user_auth, generate_token, blacklist_token
+from users.auth import user_auth
 from .models import Word, UserWord
-from .models import CustomUser as User
-from .schemas import WordInputSchema, WordOutSchema, UserWordOutSchema, UserOut, LoginIn
+from .schemas import WordInputSchema, WordOutSchema
 from .utils import fetch_word_data
 
-api = NinjaAPI()
+# from django.contrib.auth.models import User
+
 
 router = Router()
-
-
-@router.get("/user", response=UserOut, auth=user_auth)
-def fetch_user(request: HttpRequest):
-    if request.auth.is_authenticated:
-        return request.auth
-    else:
-        raise HttpError(401, "User not authenticated")
-
-
-@router.post("user/login")
-def user_login(request: HttpRequest, payload: LoginIn) -> HttpResponse:
-    try:
-        user = User.objects.get(username=payload.username)
-        if user.check_password(payload.password):
-            login(request, user)
-            return HttpResponse(generate_token(user), status=200)
-        else:
-            return HttpResponse("Wrong username or password", status=401)
-    except User.DoesNotExist:
-        return HttpResponse("Wrong username or password", status=401)
-
-
-@router.post("user/logout", auth=user_auth)
-def user_logout(request):
-    logout(request)
-    blacklist_token(request.headers.get("Authorization"))
 
 
 # TODO: write schema for UserWordSchema
--- vocabulary/schemas.py
+++ vocabulary/schemas.py
@@ -2,7 +2,6 @@ from pydantic import Field
 from ninja import ModelSchema, Schema
 from typing import Optional
 
-from vocabulary.models import CustomUser
 
 
 class WordInputSchema(Schema):
@@ -32,19 +31,3 @@ class UserWordOutSchema(Schema):
     user_note: str
 
 
-class LoginIn(ModelSchema):
-    class Config:
-        model = CustomUser
-        model_fields = ["username", "password"]
-
-
-class LogoutOut(ModelSchema):
-    class Config:
-        model = CustomUser
-        model_fields = ["username", "email", "first_name", "last_name"]
-
-
-class UserOut(ModelSchema):
-    class Config:
-        model = CustomUser
-        model_fields = ["username", "email", "first_name", "last_name"]
