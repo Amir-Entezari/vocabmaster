f073fb9386cbe19fa270ee20041b2890d81626ad
amir <mr.amirentezari@gmail.com>
2024-05-30 14:33:45 +0330

    feat(api): add users router to main API

--- config/api.py
+++ config/api.py
@@ -1,6 +1,8 @@
 from ninja import NinjaAPI
 from vocabulary.api import router as vocabulary_router
+from users.api import router as users_router
 api = NinjaAPI(
     title="Vocabulary Master",
 )
 api.add_router("", vocabulary_router)
+api.add_router("/users", users_router)
